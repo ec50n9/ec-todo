fd55681a0a6b82b628a690c78169bb8c4ca35758
ec50n9 <shelloworld@qq.com>
2022-11-06 15:23:03 +0800

    创建todo弹窗

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,41 +1,69 @@
 <template>
-  <div @scroll="handleContainerScroll" h-full overflow-scroll>
-    <RouterView v-slot="{ Component, route }">
-      <Transition name="fade" mode="out-in">
-        <Component :is="Component" :key="route.path" />
-      </Transition>
-    </RouterView>
-  </div>
-  <div
-    absolute
-    bottom-0
-    w-full
-    p-3
-    transition-all
-    duration-500
-    transform
-    :class="[showTabs ? 'translate-y-0' : 'translate-y-full']"
-  >
-    <EcTabBar
-      :bg-color="themeStore.colors.lightPrimaryColor"
-      :text-color="themeStore.colors.lightText"
-      :selected-bg-color="themeStore.colors.primaryColor"
-      :selected-text-color="themeStore.colors.lightPrimaryText"
-      :tabs="tabs"
-      @change="handleTabChange"
-      rounded-4
-    />
+  <div ref="layout">
+    <div @scroll="handleContainerScroll" h-full overflow-scroll>
+      <RouterView v-slot="{ Component, route }">
+        <Transition name="fade" mode="out-in">
+          <Component :is="Component" :key="route.path" />
+        </Transition>
+      </RouterView>
+    </div>
+    <div
+      absolute
+      bottom-0
+      w-full
+      p-3
+      transition-all
+      duration-500
+      transform
+      :class="[showTabs ? 'translate-y-0' : 'translate-y-full']"
+    >
+      <EcTabBar
+        :bg-color="themeStore.colors.lightPrimaryColor"
+        :text-color="themeStore.colors.lightText"
+        :selected-bg-color="themeStore.colors.primaryColor"
+        :selected-text-color="themeStore.colors.lightPrimaryText"
+        :tabs="tabs"
+        @change="handleTabChange"
+        rounded-4
+      />
+    </div>
+    <!-- 创建todo弹窗 -->
+    <EcModal v-model:visible="createTodoModal">
+      <div w-full p-4>
+        <div p-6 bg-white shadow-lg rounded-xl>
+          <h1 mb-4 text-xl>添加</h1>
+          <div flex flex-col gap-4>
+            <EcFloatingLabel v-model="todoData.title" place-holder="标题" />
+            <EcFloatingLabel v-model="todoData.desc" place-holder="备注" />
+          </div>
+          <div mt-4 flex justify-end gap-4>
+            <button px-4 py-2 text-gray-7 bg-gray-1 rounded-md>取消</button>
+            <button px-4 py-2 text-white bg-blue-6 rounded-md>添加</button>
+          </div>
+        </div>
+      </div>
+    </EcModal>
   </div>
 </template>
 
 <script setup lang="ts">
-import { onMounted, ref } from "vue";
+import { onMounted, reactive, ref } from "vue";
 import { useRouter } from "vue-router";
 import EcTabBar from "../../components/EcTabBar.vue";
 import { useThemeStore } from "../../store/ThemeStore";
 import { TabItem } from "../../components/EcTabBar.vue";
+import EcModal from "../../components/EcModal.vue";
+import EcFloatingLabel from "../../components/EcFloatingLabel.vue";
 
 const themeStore = useThemeStore();
+
+// 创建todo
+const layout = ref<Element>();
+const createTodoModal = ref(false);
+const todoData = reactive({
+  title: "",
+  desc: "",
+});
 
 // 底部栏
 const router = useRouter();
@@ -51,8 +79,8 @@ const tabs: TabItem[] = [
     path: "/todo",
     selectedIcon: "i-akar-icons:plus",
     selectedText: "添加",
-    selectedFunc: () => {
-      console.log("hello, world");
+    selectedFunc: (ele) => {
+      createTodoModal.value = true;
     },
   },
   {
--- src/store/TodoStore.ts
+++ src/store/TodoStore.ts
@@ -10,6 +10,18 @@ type Todo = {
 export const useTodoStore = defineStore("todo", {
   persist: true,
   state: () => ({
-    todoList: [] as Todo[],
+    todoList: [
+      {
+        title: "吃早餐",
+        date: new Date(),
+        duration: 100,
+        finished: false,
+      },
+    ] as Todo[],
   }),
+  actions: {
+    addTodo(todo: Todo) {
+      this.todoList.push(todo);
+    },
+  },
 });
