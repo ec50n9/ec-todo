5637e97c4f98cfc3d84cc2450570ccb8e04ae4ec
ec50n9 <shelloworld@qq.com>
2022-11-08 18:27:20 +0800

    todo列表倒序插入

--- src/store/TodoStore.ts
+++ src/store/TodoStore.ts
@@ -3,18 +3,12 @@ import { defineStore } from "pinia";
 export const useTodoStore = defineStore("todo", {
   persist: true,
   state: () => ({
-    todoList: [
-      {
-        title: "吃早餐",
-        date: new Date(),
-        duration: 100,
-        finished: false,
-      },
-    ] as Todo[],
+    todoList: [] as Todo[],
   }),
   actions: {
     addTodo(todo: Todo) {
-      this.todoList.push(todo);
+      // this.todoList.push(todo);
+      this.todoList.unshift(todo);
     },
   },
 });
