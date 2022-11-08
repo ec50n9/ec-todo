90eab530dfbcfbf08a751a8d9000faf5dccfb473
ec50n9 <shelloworld@qq.com>
2022-11-08 14:28:51 +0800

    todo item颜色跟着类型变化

--- src/pages/todo/todo-item.vue
+++ src/pages/todo/todo-item.vue
@@ -1,31 +1,41 @@
 <template>
   <li
-    :class="['todo-item', finished ? 'todo-item--finished' : '']"
+    :class="['todo-item', todo.finished ? 'todo-item--finished' : '']"
     py-3
     flex
     items-center
     gap-4
   >
-    <EcCheckBox w-6 h-6 :checked="finished" @change="handleChangeStatus" />
+    <EcCheckBox
+      w-6
+      h-6
+      :color="todoColors[todo.type]"
+      :checked="todo.finished"
+      @change="handleChangeStatus"
+    />
     <p flex-grow-1>
-      <span class="todo-item__title">{{ title }}</span>
+      <span class="todo-item__title">{{ todo.title }}</span>
     </p>
   </li>
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue";
 import EcCheckBox from "../../components/EcCheckBox.vue";
 
 const props = defineProps<{
-  title: string;
-  finished: boolean;
+  todo: Todo;
 }>();
 const emit = defineEmits(["changeStatus"]);
 
 function handleChangeStatus(newValue: boolean) {
   emit("changeStatus", newValue);
 }
+
+const todoColors = {
+  todo: "#1D4ED8",
+  tomato: "#B91C1C",
+  potato: "#B45309",
+};
 </script>
 
 <style>
@@ -41,7 +51,7 @@ function handleChangeStatus(newValue: boolean) {
   top: calc(50% - 1px);
   height: 2px;
   width: 0;
-  background-color: #6b7280;
+  background-color: v-bind("todoColors[todo.type]");
   transition: width 0.3s ease;
 }
 
