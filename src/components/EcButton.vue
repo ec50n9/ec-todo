417f969ec587ea036baf05537d07076a8d987c49
ec50n9 <shelloworld@qq.com>
2022-11-08 14:28:14 +0800

    按钮增加类型

--- src/components/EcButton.vue
+++ src/components/EcButton.vue
@@ -1,9 +1,22 @@
 <template>
-  <button px-4 py-2 text-white bg-blue-6 rounded-md select-none>
+  <button px-4 py-2 rounded-md select-none :class="additionClass[type]">
     <slot />
   </button>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+type Props = {
+  type: "default" | "primary" | "error";
+};
+const props = withDefaults(defineProps<Props>(), {
+  type: "default",
+});
+
+const additionClass = {
+  default: "text-gray-7 bg-gray-1",
+  primary: "text-white bg-blue-6",
+  error: "text-white bg-red-6",
+};
+</script>
 
 <style scoped></style>
